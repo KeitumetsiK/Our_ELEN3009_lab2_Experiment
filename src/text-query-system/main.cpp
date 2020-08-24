3c7a8ecb983bc49bbf5bd742a31c7afb02686fbb
Elijah Kadimba <ekadimba10@gmail.com>
2020-08-24 22:49:54 +0200

    Exercise 5.4

--- src/text-query-system/main.cpp
+++ src/text-query-system/main.cpp
@@ -1,12 +1,57 @@
 // main.cpp
 // Main for TextQueryApp
 
-#include <iostream>
+
+#include "Word.h"
+#include "Paragraph.h"
+#include "Line.h"
+#include "FileReader.h"
+
+#include<iostream>
+#include<string>
+#include<vector>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    auto paragraph = Paragraph{};
+
+    string fileName = "", word = "";
+    cout << "Please enter file name: ";
+    cin >> fileName;
+    cout << endl;
+
+    auto filereader = FileReader{fileName};
+    filereader.readFileInto(paragraph);
+
+    start:
+    cout << "Please enter a word to search for or ""."" to quit: ";
+    cin >> word;
+    cout << endl;
+
+    if(word == ".")
+    {
+        return 0;
+    }
+
+    auto[found, line_numbers] = paragraph.contains(Word{word});
+
+    if (!found){cout << "Word not found" << endl;}
+
+    else
+    {
+        cout << "Word found: ";
+
+        for (int i = 0; i < line_numbers.size(); i++)
+        {
+            if (i==0){cout << "Line " << line_numbers[i] << endl;}
+            else{cout << "            Line " << line_numbers[i] << endl;}
+        }
+    }
+    cout << endl;
+
+    goto start;
+
     return 0;
 }
